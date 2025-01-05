d66e88d4a182ca08d4f7539cdcebbb6222bfc946
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-05 19:39:45 +0530

    FEAT: Created Contact Us Page with Contact Form and Map

---
+++ src/features/contact/ContactForm.jsx
@@ -0,0 +1,90 @@
+import { useForm } from "react-hook-form";
+import FormRow from "../../ui/FormRow";
+import Button from "../../ui/Button";
+import Heading from "../../ui/Heading";
+import toast from "react-hot-toast";
+
+function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {},
+  });
+
+  async function onSubmit(data) {
+    toast.success("Thank you for sending us a message ! 😁");
+    reset();
+  }
+  return (
+    <div className="flex flex-col items-center justify-items-center gap-8">
+      <Heading as="h4">Drop us a message</Heading>
+
+      <form
+        className="grid w-60 grid-cols-1 content-between items-center laptopS:w-80"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <FormRow label="Name" error={errors?.name?.message}>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your name"
+            autoComplete="name"
+            // disabled={isPending}
+            {...register("name", { required: "*This field is required" })}
+          />
+        </FormRow>
+
+        <FormRow label="Email address" error={errors?.email?.message}>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            autoComplete="username"
+            // disabled={isPending}
+            {...register("email", { required: "*This field is required" })}
+          />
+        </FormRow>
+
+        <FormRow label="Phone Number" error={errors?.phone?.message}>
+          <input
+            type="tel"
+            id="phone"
+            placeholder="Enter your phone number"
+            autoComplete="username"
+            // disabled={isPending}
+            {...register("phone", { required: "*This field is required" })}
+          />
+        </FormRow>
+
+        <FormRow label="Subject" error={errors?.subject?.message}>
+          <input
+            type="text"
+            id="subject"
+            placeholder="Enter the subject"
+            autoComplete="username"
+            // disabled={isPending}
+            {...register("subject", { required: "*This field is required" })}
+          />
+        </FormRow>
+
+        <FormRow label="Description">
+          <textarea
+            type="text"
+            placeholder="Enter your message"
+            id="message"
+            {...register("message")}
+          />
+        </FormRow>
+
+        <FormRow>
+          <Button variation="primary">Submit</Button>
+        </FormRow>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,5 +1,13 @@
+import ContactForm from "../features/contact/ContactForm";
+import Map from "../features/contact/Map";
+
 function ContactUsPage() {
-  return <div>Contact Us</div>;
+  return (
+    <div className="flex flex-col items-center justify-items-center gap-8 py-5 tablet:m-5 tablet:flex-row tablet:justify-evenly">
+      <ContactForm />
+      <Map />
+    </div>
+  );
 }
 
 export default ContactUsPage;
