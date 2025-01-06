2da2ce554ae7b9313cc57f7676f5963b9c6d7fed
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-06 17:40:30 +0530

    FEAT: Created BreadCrumb Trail for Navigation

---
+++ src/ui/BreadCrumbTrail.jsx
@@ -0,0 +1,31 @@
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
+
+function BreadCrumbTrail() {
+  const { pathname } = useLocation();
+
+  const paths = pathname.split("/");
+  return (
+    <div className="mx-5 my-5 flex items-center gap-2 text-sm text-primary-100">
+      <Button to="/">HOME</Button>
+      <span> {`>`} </span>
+      {paths.map((path, i) => {
+        if (!path) return null;
+        if (path && i < paths.length - 1) {
+          return (
+            <Button key={path} to={path}>
+              {path.toUpperCase()}
+              <span> {`>`} </span>
+            </Button>
+          );
+        }
+        if (path && i <= paths.length - 1) {
+          return <Button key={path}>{path.toUpperCase()}</Button>;
+        }
+        return null;
+      })}
+    </div>
+  );
+}
+
+export default BreadCrumbTrail;
