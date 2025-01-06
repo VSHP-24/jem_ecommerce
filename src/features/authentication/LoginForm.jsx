9b7ce6446b02c3b6a2be3cb4fb9b81df1237e410
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-06 19:21:27 +0530

    FEAT: Created Form Page for Logging In Users

---
+++ src/features/authentication/LoginForm.jsx
@@ -0,0 +1,68 @@
+import { useForm } from "react-hook-form";
+import FormRow from "../../ui/FormRow";
+import Button from "../../ui/Button";
+import { NavLink } from "react-router-dom";
+import { useLogin } from "./useLogin";
+
+function LoginForm() {
+  const { login, isPending } = useLogin();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: { email: "admin@jem.com", password: "passwordforadmin@123" },
+  });
+
+  async function onSubmit(data) {
+    login({ ...data }, { onSuccess: () => reset() });
+  }
+
+  return (
+    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Email address" error={errors?.email?.message}>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          autoComplete="username"
+          disabled={isPending}
+          {...register("email", { required: "*This field is required" })}
+        />
+      </FormRow>
+
+      <FormRow label="Password" error={errors?.password?.message}>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+          autoComplete="current-password"
+          disabled={isPending}
+          {...register("password", { required: "*This field is required" })}
+        />
+      </FormRow>
+
+      <NavLink
+        className="text-base font-semibold text-blue-800 hover:font-bold hover:underline focus:underline active:underline"
+        to="/forgot-password"
+      >
+        Forgot Password ?
+      </NavLink>
+
+      <NavLink
+        className="text-base font-semibold text-blue-800 hover:font-bold hover:underline focus:underline active:underline"
+        to="/new-customer"
+      >
+        New Customer ?
+      </NavLink>
+
+      <FormRow>
+        <Button variation="primary">Login</Button>
+      </FormRow>
+    </form>
+  );
+}
+
+export default LoginForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,15 @@
+import LoginForm from "../features/authentication/LoginForm";
+import Heading from "../ui/Heading";
+
 function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <Heading as="h4" styles="text-lg tablet:text-xl self-center  ">
+        Login to your account
+      </Heading>
+      <LoginForm />
+    </div>
+  );
 }
 
 export default LoginPage;
