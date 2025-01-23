ac7a9efa97d12e29f2b0ecd474d09939acff37b6
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-23 19:44:50 +0530

    FIX: Bug fixed to navigate to login page if the user logged out within protected routes

--- src/ui/ProtectedRoutes.jsx
+++ src/ui/ProtectedRoutes.jsx
@@ -1,16 +1,19 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { useUser } from "../features/authentication/useUser";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getUserDetails } from "../features/user/userSlice";
 
 function ProtectedRoutes() {
   const navigate = useNavigate();
+  const userDetails = useSelector(getUserDetails);
   const { isPending, isAuthenticated } = useUser();
 
   useEffect(
     function () {
-      if (!isAuthenticated && !isPending) navigate("/login");
+      if (!isAuthenticated && !userDetails.id && !isPending) navigate("/login");
     },
-    [isAuthenticated, isPending, navigate],
+    [isAuthenticated, isPending, navigate, userDetails.id],
   );
 
   if (!isPending && isAuthenticated) return <Outlet />;
