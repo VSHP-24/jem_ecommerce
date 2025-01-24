d419626c2f64ea1f5cb58494c0f133bf7e3d968a
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-24 16:14:11 +0530

    FEAT: Created Customer Contact Details Form in User Profile Page

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -1,5 +1,15 @@
+import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
+import MyProfileForm from "../features/user/MyProfileForm";
+import UserContactDetailsForm from "../features/user/UserContactDetailsForm";
+
 function UserProfilePage() {
-  return <div>User Profile</div>;
+  return (
+    <div className="flex max-w-fit flex-col gap-16 justify-self-center">
+      <MyProfileForm />
+      <UpdatePasswordForm />
+      <UserContactDetailsForm />
+    </div>
+  );
 }
 
 export default UserProfilePage;
