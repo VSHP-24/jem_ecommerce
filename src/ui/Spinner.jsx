b8df06c4deae4a88d1af0669d3386488a07d49ad
VSHP-24 <srihariprasadsniper@gmail.com>
2025-01-27 13:25:46 +0530

    BUILD: Created a Spinner & Loader Component

---
+++ src/ui/Spinner.jsx
@@ -0,0 +1,7 @@
+function Spinner() {
+  return (
+    <div className="h-6 w-6 animate-spin justify-self-center rounded-full border-4 border-dashed border-black"></div>
+  );
+}
+
+export default Spinner;
